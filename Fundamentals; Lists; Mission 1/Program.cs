7a7ad0e60a3799e8329a7d54159c38049732a7ca
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-06-08 16:32:16 +0200

    Add project files.

---
+++ Fundamentals; Lists; Mission 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentals__Lists__Mission_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
